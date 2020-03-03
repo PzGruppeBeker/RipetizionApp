e4d591345a93d2c73e4acbcb76243c74e2d87a3c
Jackoopa <53277420+Jackoopa@users.noreply.github.com>
2020-03-03 13:40:00 +0100

    navigation menu   hamburger icon added   transparent status bar added

--- app/src/main/java/com/example/ripetizionapp/MainActivity.java
+++ app/src/main/java/com/example/ripetizionapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ripetizionapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -10,11 +14,22 @@ import android.os.Bundle;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
     /**
         //Ottieni riferimeno a database Firebase.
@@ -31,5 +46,14 @@ public class MainActivity extends AppCompatActivity {
         Insegnante i = new Insegnante("Bo","Ma",m);
         Ref.child("Prof").setValue(i);
      */
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
