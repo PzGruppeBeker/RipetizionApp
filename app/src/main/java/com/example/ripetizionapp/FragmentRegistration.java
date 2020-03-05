58c345151d25dece0f5ede63a0af0eb31c70250f
PzGruppeBeker <49306727+PzGruppeBeker@users.noreply.github.com>
2020-03-05 17:46:56 +0100

    Aggancio Firestore.

--- app/src/main/java/com/example/ripetizionapp/FragmentRegistration.java
+++ app/src/main/java/com/example/ripetizionapp/FragmentRegistration.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import androidx.fragment.app.Fragment;
 
 public class FragmentRegistration extends Fragment {
@@ -17,12 +19,26 @@ public class FragmentRegistration extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_registration, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_registration, container, false);
 
         Button confirm = rootView.findViewById(R.id.button_confirm);
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                TextInputLayout Viewname = rootView.findViewById(R.id.text_input_name);
+                TextInputLayout Viewsurname = rootView.findViewById(R.id.text_input_surname);
+                TextInputLayout Viewemail = rootView.findViewById(R.id.text_input_email);
+                TextInputLayout Viewpassword = rootView.findViewById(R.id.text_input_password);
+                TextInputLayout Viewsubjects = rootView.findViewById(R.id.text_input_subject);
+
+                String nome = Viewname.getEditText().getText().toString().trim();
+                String cognome = Viewsurname.getEditText().getText().toString().trim();
+                String email = Viewemail.getEditText().getText().toString().trim();
+                String password = Viewpassword.getEditText().getText().toString().trim();
+                String materie = Viewsubjects.getEditText().getText().toString().trim();
+
+
                 Toast.makeText(getContext(), "CONFIRM INPUT RECEIVED", Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/main/java/com/example/ripetizionapp/SupportMethods.java
+++ app/src/main/java/com/example/ripetizionapp/SupportMethods.java
@@ -1,5 +1,17 @@
 package com.example.ripetizionapp;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+import androidx.annotation.NonNull;
+
 public class SupportMethods {
     public static String mailtoDB (String s){
         String n = s.replace('.',':');
@@ -8,5 +20,11 @@ public class SupportMethods {
     public static String mailfromDB (String s){
         String n = s.replace(':','.');
         return n;
+    }
+
+    public static boolean checkEmail (final String email) {
+        FirebaseFirestore ff = FirebaseFirestore.getInstance();
+        DocumentReference docRef = ff.collection("insegnanti").document(email);
+        if (docRef.get()==null)
     }
 }
